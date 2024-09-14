7af2cf6d896ccd673b1bd49fd92072da25105308
Andrew Marra <marraand@outlook.com>
2024-09-14 15:57:57 -0500

    1.1.2 : Fix directory name

--- code-quick-draw/FrontEnd/src/App.js
+++ code-quick-draw/FrontEnd/src/App.js
@@ -1,14 +1,25 @@
 import './App.css';
 import ButtonAppBar from './components/ButtonAppBar';
 import Lobby from './components/Lobby';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import AboutPage from './components/AboutPage';
+
 
 function App() {
   return (
-    <><div className="App">
+    <div className="App">
       <ButtonAppBar />
-    </div><div className="App">
-        <Lobby />
-      </div></>
+      <Lobby />
+
+      <Router>
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route path="/about" component={AboutPage} />
+          <Route component={NotFound} />
+        </Switch>
+      </Router>
+
+    </div>
     
   );
 }
---
+++ code-quick-draw/FrontEnd/src/components/AboutPage.jsx
@@ -0,0 +1,86 @@
+import React from 'react';
+import { Link } from 'react-router-dom'; // Assuming you're using React Router for navigation
+import 'Style.css'
+
+const AboutPage = () => {
+  return (
+    <div className="AboutGround">
+      <div className="fade-in">
+        <div className="banner">
+          <img
+            className="logo"
+            src="images/CodeQuickdrawLogo.png"
+            alt="CodeQuickdrawLogo Logo"
+          />
+        </div>
+      </div>
+
+      <div className="buttonRow">
+        <div className="fade-in">
+          <button className="submit">
+            <Link className="link" id="link" to="/">
+              Home
+            </Link>
+          </button>
+          <button className="submit">
+            <Link className="link" id="link" to="/play">
+              Play
+            </Link>
+          </button>
+          <button className="submit">
+            <Link className="link" id="link" to="/about">
+              About Us
+            </Link>
+          </button>
+          <button className="submit">
+            <Link className="link" id="link" to="/contacts">
+              Contacts
+            </Link>
+          </button>
+        </div>
+      </div>
+
+      <div className="heading">
+        <div className="fade-in">
+          <h1>Meet the Team</h1>
+        </div>
+      </div>
+
+      <div className="TheBoys">
+        <div className="fade-in">
+          <img src="images/thaBoys.jpg" alt="The Boys" />
+        </div>
+      </div>
+
+      <div className="heading">
+        <div className="fade-in">
+          <h3>
+            Our team is comprised of computer science students who enjoy
+            pushing themselves to develop coding skills. The Idea of NutriSync
+            emerged as a way for our collective ambition to explore and excel in
+            diverse areas of computer science. Through this project, we have
+            embraced challenges that span a wide spectrum of technological
+            competencies and have grown as problem solvers and innovators.
+          </h3>
+        </div>
+      </div>
+
+      <div className="Tech">
+        <div className="fade-in">
+          <img src="images/TexasTech.png" alt="Go Red Raiders!" />
+        </div>
+      </div>
+
+      <div className="Whitacre">
+        <div className="fade-in">
+          <img
+            src="images/Whitacare.png"
+            alt="Whitacre Jr. College of Engineering"
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default AboutPage;
